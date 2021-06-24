416090855ab5eb56647bcd6d659a13bfd98121f5
AAashik519 <64531121+AAashik519@users.noreply.github.com>
2021-06-24 21:51:43 +0600

    fire-auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,121 @@
 import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config'
+import { useState } from 'react';
 
+
+firebase.initializeApp(firebaseConfig);
 function App() {
+  const [user ,setUser]=useState({
+    isSignIN :false,
+    name :'',
+    email :'',
+    photo :'',
+    password:''
+  
+
+  })
+   const provider = new firebase.auth.GoogleAuthProvider();
+   const handleSignIn=()=>{
+    firebase.auth().signInWithPopup(provider)
+     .then((res) =>{
+      
+       const {displayName ,photoURL ,email} =res.user
+         console.log(displayName ,photoURL ,email  );
+
+         const signInUser= {
+           isSignIN: true,
+           name:displayName,
+           email:email,
+           photo:photoURL
+         }
+         setUser(signInUser)
+
+     })
+     .catch((err) =>{
+       console.log(err.message);
+     })
+   
+     
+   }
+
+   const handleSignOut =()=>{
+    // const signOutUser ={
+    //   isSignIN :false
+    // }
+    // setUser(signOutUser)
+    firebase.auth().signOut()
+    .then((res) => {
+       const signOutUser={
+        isSignIN :false,
+        name :'',
+        email :'',
+        photo :'',
+        password:''
+      
+       }
+       console.log(res);
+       setUser(signOutUser)
+    }).catch((err) => {
+      console.log(err.message);
+    });
+   }
+
+
+   const handleSubmit =()=>{
+
+   }
+   const handleChange=(e)=>{
+      let isFormValid ;
+      if(e.target.name==='email'){
+             isFormValid = /\S+@\S+\.\S+/.test(e.target.value)
+      }
+      if(e.target.name==='password'){
+          const isPasswordValid = e.target.value.length >6
+          const passwordHasNumber =/\d{1}/.test(e.target.value)
+          isFormValid =isPasswordValid &&  passwordHasNumber;
+      }
+      if(isFormValid){
+          const newUserInfo ={...user}
+          newUserInfo[e.target.name] = e.target.value ;
+          setUser(newUserInfo)
+      }
+   }
+
+    
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { user.isSignIN ?
+        <button onClick ={handleSignOut}> Sign out</button> : <button onClick ={handleSignIn}> Sign In</button>
+      }
+       {
+         user.isSignIN &&  <div>
+           <p> welcome , {user.name} </p>
+           <p> Your email Address , {user.email} </p>
+           <img src={user.photo} alt="email image" />
+            </div>
+}
+
+      <form  onSubmit={handleSubmit}>
+        
+        <p> Email :{user.email} </p>
+        <p>password {user.password} :</p>
+        <p>Name :{user.name}</p>
+      <h1> Our Own Authintication</h1>
+      <input type="text" name='name' onBlur={handleChange}  placeholder='Your name' />
+      <br />
+       <input type="email" name="email" onBlur={handleChange} placeholder='your email address' required/> <br />
+       <input type="password" name="password" onBlur={handleChange} required placeholder ='your password'/>
+       <br />
+      <input type="submit" value="Submit" />
+      </form>
+
     </div>
   );
 }
 
 export default App;
+
---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBhUVuqm8fRRpIAg-MwcK2q8xbP9p9QoU0",
+    authDomain: "ema-john-simple-f6e77.firebaseapp.com",
+    projectId: "ema-john-simple-f6e77",
+    storageBucket: "ema-john-simple-f6e77.appspot.com",
+    messagingSenderId: "174412863942",
+    appId: "1:174412863942:web:665fc4515957d67c293ac7"
+  };
+
+   export default firebaseConfig ;
